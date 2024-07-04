fe3b9928d865b004fc37f6fd2a1e292b13d59c56
Meffodiiy <meffodiiy@gmail.com>
2024-07-04 21:07:39 +0200

    micro fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import './page.sass'
 import Logo from '@lib/components/Logo'
 import * as Texts from '@lib/constants/texts'
 import Button from '@lib/components/Button'
@@ -11,7 +12,7 @@ export default function HomePage () {
     <main className="home-page">
       <Logo variant="big"/>
       <h3>{Texts.APP_DESCRIPTION}</h3>
-      <Button onClick={() => router.push('/login')}>
+      <Button onClick={() => router.push('/login')} variant="ok">
         {Texts.LOG_IN}
       </Button>
     </main>
